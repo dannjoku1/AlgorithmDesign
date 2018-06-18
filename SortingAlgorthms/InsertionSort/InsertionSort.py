00b818575d65c09528c72f6646f1ee56d750a7a9
Dan Njoku <dannjoku1@gmail.com>
2018-06-18 16:31:06 -0400

    Adding three new algorithms
    Implementation of Heap, Merge and Selection Sort.

--- SortingAlgorthms/InsertionSort/InsertionSort.py
+++ SortingAlgorthms/InsertionSort/InsertionSort.py
@@ -1,30 +1,25 @@
-# Simple Python program for implementation of Bubble Sort Algorithm
+# Simple Python program for implementation of the Insertion Sort Algorithm
 
-def bubbleSort(arr):
-    n = len(arr)
-    # if array length is less than 2
-    # then return the array
-    if (n < 2):
-        return arr
+# Function to do insertion sort
+def insertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
 
-    # traverse through all array elements
-    for i in range(n):
+        key = arr[i]
 
-        # i is equivalent to the elements already in place
-        for j in range(0, n - i - 1):
-
-            # traverse the array from 0 to remaining unsorted array
-            # swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
 
-# test code
-arr = [64, 12, 1, 7, 105, 66, 99, 5, 18, 12, 102, 11, 0, 6, -1]
-
-bubbleSort(arr)
-
+# Driver code to test above
+arr = [12, 11, 13, 5, 6, 34, 56, 17, 88, 17, 15]
+insertionSort(arr)
 print("Sorted array is:")
 for i in range(len(arr)):
-    print(arr[i])
+    print("%d" % arr[i]),
