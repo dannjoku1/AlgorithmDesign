f747b674506d101d413580abe786820795735ab4
Dan Njoku <dannjoku1@gmail.com>
2018-06-19 19:50:56 -0400

    adding search algorithms
    introducing binary search

---
+++ Search/BinarySearch.py
@@ -0,0 +1,24 @@
+# Simple Python program for implementation of Bubble Sort Algorithm
+# Binary search works for a sorted array.
+# Note: The code logic is written for an array sorted in
+#       increasing order.
+# T(n): O(log n)
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    found = False
+    while (low <= high and not found):
+        mid = low + (high - low) // 2
+        if arr[mid] == target:
+            found = True
+        else:
+            if target < arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+    return found
+
+
+print(binary_search([1, 2, 3, 5, 8, 10, 11, 12, 17, 21], 6))
+print(binary_search([1, 2, 3, 5, 8, 11, 16, 99], 5))
